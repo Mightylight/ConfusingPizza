aa8567ba90727d370a0423ad5fb4ec15d2dc9a78
Bas Meddeler <bejmeddeler@gmail.com>
2024-02-16 11:44:43 +0100

    Small fix

--- Assets/Assets/Designer/UI/Scripts/Scene_Transition.cs
+++ Assets/Assets/Designer/UI/Scripts/Scene_Transition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Scene_Transition : MonoBehaviour
 {
     public class SceneLoader : MonoBehaviour
@@ -9,15 +10,16 @@ public class Scene_Transition : MonoBehaviour
         public void LoadScene()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-     }
+        }
 
-    public void ResetGame()
-    {
-        SceneManager.LoadScene("Main Menu Scene");
-    }
+        public void ResetGame()
+        {
+            SceneManager.LoadScene("Main Menu Scene");
+        }
 
-    public void ExitGame()
-    {
-        Application.Quit();
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
     }
 }

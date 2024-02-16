6a3859bb4d3b6290a58746b0ea2f52f6cca074d9
Bas Meddeler <bejmeddeler@gmail.com>
2024-02-16 11:54:03 +0100

    Added the cursed prefabs

--- Assets/Assets/Designer/UI/Scripts/Scene_Transition.cs
+++ Assets/Assets/Designer/UI/Scripts/Scene_Transition.cs
@@ -2,24 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class Scene_Transition : MonoBehaviour
 {
-    public class SceneLoader : MonoBehaviour
-    {
-        public void LoadScene()
-        {
+    public void LoadScene()
+     {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+     }
 
-        public void ResetGame()
-        {
-            SceneManager.LoadScene("Main Menu Scene");
-        }
+    public void ResetGame()
+    {
+        SceneManager.LoadScene("Main Menu Scene");
+    }
 
-        public void ExitGame()
-        {
-            Application.Quit();
-        }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
